f34a53642f873e004a592ba3aa2f377fd0004cde
Aleed <aleed@hey.com>
2022-07-16 19:14:46 -0500

    ensure defc compiles last return value even if it's an eval expr

--- src/rectify/core/component.clj
+++ src/rectify/core/component.clj
@@ -1,7 +1,7 @@
 (ns rectify.core.component
   (:require
    [rectify.core.spec :as spec]
-   [rectify.core.hiccup :as hiccup]))
+   [rectify.core.hiccup :as hic]))
 
 (defn widget-fn*
   "Generate widget as a fn expr."
@@ -12,9 +12,12 @@
       (println "(Warning) Extra hiccup expressions in component" (str "'" name "'") "are ignored."))
     ;; todo may have to gensym params
     `(fn ~name ~params
-       ~@(when (seq evals) evals)
-      ;;  note: splicing to remove nils if only eval exprs were declared; as last eval should be returned child.
-       ~@(when (seq hiccups) [(hiccup/compile (last hiccups) compile-opts)]))))
+       ~@(let [exprs (cond-> []
+                       (seq evals) (into evals)
+                       (seq hiccups)  (conj (last hiccups)))]
+          ;;  make sure last return value is compiled, even if it's a list expr
+           (update exprs (- (count exprs) 1) #(hic/compile % compile-opts))))))
+
 
 (defn generate* [decls compile-opts]
   (let [{:keys [name docstr] :as component-map} (spec/conform decls :rec/component)]
@@ -22,8 +25,7 @@
        ~(widget-fn* component-map compile-opts))))
 
 (comment
-  (defn emitter [tag props]
-    `(~tag ~props))
+  (defn emitter [tag props [& children]] `(~tag ~props ~@children))
 
   (def opts {:parsers [[keyword? {:tag #(-> % name symbol)}]]
              :emitter emitter})
@@ -35,6 +37,7 @@
   (generate* '(name [] [:a "b"]) opts)
 
   ;;  without any hiccup, last eval expr acts as child
+  ;; note: will throw since not registered in allowed sexp list
   (generate* '(name [] (component)) opts)
 
   ;; extra child prints warning
